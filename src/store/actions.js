accc70b2f141fff0642a574a8ce86bac162f9522
coolbambyk <cool.bambyk@gmail.com>
2021-11-30 11:13:37 +0200

    fix vuex+axios

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,13 +1,44 @@
+import axios from "axios";
+
 export default {
-    createList(context, payload) {
-      context.commit("createNewList", payload);
+    async createList(context, payload) {
+      context.state.lastListId = context.state.lastListId+1;
+      const list = {
+        _id: Math.floor(Math.random() * 61900000000000000000),
+        id: context.state.lastListId,
+        name: payload
+      };
+      await axios
+        .post('https://trellobackendnodejs.herokuapp.com/api/lists', null, {
+          params: list
+        })
+      context.commit("createNewList", list);
     },
     deleteList(context, payload) {
-        context.commit("deleteList", payload);
+      context.state.lists.map((list, index) => {
+        if(list.id === payload) {
+          axios.delete(`https://trellobackendnodejs.herokuapp.com/api/lists/${list._id}`)
+          context.commit("deleteList", index);
+        }
+      })
+        
     },
-    createCard(context, payload) {
-      context.commit("createNewCard", payload);
-      context.commit("fetchCards");
+    async createCard(context, payload) {
+      context.state.lastCardId = context.state.cards.length
+
+      const card = {
+        listId: payload.listId,
+        id: context.state.lastCardId,
+        edited: Date.now(),
+        name: payload.name,
+        _id: payload._id
+      };
+      
+      await axios
+        .post('localhost:5000/api/cards', {
+           card
+        })
+      context.commit("createNewCard", card);
     },
     toggleOverlay(context) {
       context.commit("toggleOverlay");
@@ -17,15 +48,21 @@ export default {
     },
     saveCard(context, payload) {
       context.commit("saveCard", payload);
-      context.commit("fetchCards");
     },
     deleteCard(context, payload) {
       context.commit("deleteCard", payload);
     },
-    fetchCards(context) {
-      context.commit("fetchCards");
+    async fetchCards(context) {
+      await axios
+        .get('https://trellobackendnodejs.herokuapp.com/api/cards')
+        .then(response => context.state.cards = response.data)
+        .catch(e => console.log(e));
     },
-    fetchLists(context) {
+    async fetchLists(context) {
+      await axios
+        .get('https://trellobackendnodejs.herokuapp.com/api/lists')
+        .then(response => context.state.lists = response.data)
+        .catch(e => console.log(e));
       context.commit("fetchLists");
     },
     changeCard(context, payload) {
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,43 +2,13 @@ import axios from "axios";
 
 export default {
     createNewList(state, payload) {
-      state.lastListId = state.lastListId+1;
-      const list = {
-        id: state.lastListId,
-        name: payload,
-        _id: Math.floor(Math.random() * 61900000000000000000)
-      };
-      state.lists.push(list);
-      axios
-        .post('https://trellobackendnodejs.herokuapp.com/api/lists', null, {
-          params: list
-        })
+      state.lists.push(payload);
     },
     deleteList(state, payload) {
-      state.lists.map((list, index) => {
-        if(list.id === payload) {
-          axios.delete(`https://trellobackendnodejs.herokuapp.com/api/lists/${list._id}`)
-          state.lists.splice(index, 1);
-        }
-      })
-      
+          state.lists.splice(payload, 1);
     },
     createNewCard(state, payload) {
-      state.lastCardId = state.cards.length
-
-      const card = {
-        listId: payload.listId,
-        id: state.lastCardId,
-        edited: Date.now(),
-        name: payload.name,
-        _id: payload._id
-      };
-      
-      axios
-        .post('https://trellobackendnodejs.herokuapp.com/api/cards', null, {
-          params: card
-        })
-
+      state.cards.push(payload)
     },
     toggleOverlay(state) {
       state.overlay = !state.overlay;
@@ -47,9 +17,9 @@ export default {
       state.currentData = payload;
     },
     saveCard(state, payload) {
-      state.cards = state.cards.map((card) => {
+      state.cards = state.cards.map(async (card) => {
         if (card.id === payload.id) {
-          axios.put('https://trellobackendnodejs.herokuapp.com/api/cards', null, {
+          await axios.put('https://trellobackendnodejs.herokuapp.com/api/cards', null, {
             params: {
               _id: card._id,
               name: payload.name,
@@ -62,32 +32,22 @@ export default {
       });
     },
     deleteCard(state, payload) {
-      state.cards.filter((card) => payload.id === card.id).map((card) => axios
+      state.cards.filter((card) => payload.id === card.id).map(async(card) => await axios
         .delete(`https://trellobackendnodejs.herokuapp.com/api/cards/${card._id}`))
       const indexToDelete = state.cards
         .map((card) => card.id)
         .indexOf(payload.id);
       state.cards.splice(indexToDelete, 1);
     },
-    async fetchCards(state) {
-      await axios
-        .get('https://trellobackendnodejs.herokuapp.com/api/cards')
-        .then(response => state.cards = response.data)
-        .catch(e => console.log(e));
-    },
-    async fetchLists(state) {
-      await axios
-        .get('https://trellobackendnodejs.herokuapp.com/api/lists')
-        .then(response => state.lists = response.data)
-        .catch(e => console.log(e));
+    fetchLists(state) {
       state.lists.length === 0 
         ? null
         : state.lastListId = Math.max.apply(Math, state.lists.map((o) => o.id ))
     },
     changeCard(state, payload) {
-      state.cards = state.cards.map((card) => {
+      state.cards = state.cards.map(async(card) => {
         if (card._id === payload.startListId) {
-          axios.put('https://trellobackendnodejs.herokuapp.com/api/cards', null, {
+          await axios.put('https://trellobackendnodejs.herokuapp.com/api/cards', null, {
             params: {
               _id: payload.startListId,
               listId: payload.endListId
